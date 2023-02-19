04e35e98d9e26dc97ee67d84f7a7d75d0662973b
Sofia B. Manzano <sofiabmanzano@gmail.com>
2023-02-19 18:48:33 +0100

    filter not working properly

--- app/home/ButtonFilter.tsx
+++ app/home/ButtonFilter.tsx
@@ -1,8 +1,14 @@
+"use client";
 import Button from "../components/Button";
-import MovieType from "../components/types"
 export function ButtonFilter(props: any) {
+  
   const handleComedyFilter = () => {
-    props.handleSetGenres([props.genre])
+    if (!props.isFiltered) {
+      props.handleSetGenres([props.genre]);
+    } else {
+      props.handleSetGenres(props.allGenres)
+    }
+    props.toggleFiltered()
   };
   return <Button onClick={handleComedyFilter}>{props.genre.name}</Button>;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,79 +1,10 @@
-"use client";
-import { useEffect, useState } from "react";
-import Button from "./components/Button";
-import ContainerImg from "./components/ContainerImg";
-import PosterImg from "./components/PosterImg";
-import GenreType from "./components/types";
 //components
-import { ButtonFilter } from "./home/ButtonFilter";
-import { ListOfMovies } from "./home/ListOfMovies";
-// interface Movie {
-//   title: string;
-//   description: string;
-//   genre: string;
-//   highlighted: boolean;
-//   id: string;
-//   poster: string;
-//   rating: number;
-//   thumbnail: string;
-// }
-
-// const fetchListMovies = async (token: string | null) => {
-//   var myHeaders = new Headers();
-
-//   myHeaders.append("Accept", "application/json");
-//   myHeaders.append("Authorization", `Bearer ${token}`);
-
-//   var requestOptions = {
-//     method: "GET",
-//     headers: myHeaders,
-//   };
-
-//   return fetch(
-//     "https://kata.conducerevel.com/films/movies",
-//     requestOptions
-//   ).then((response) => response.json());
-// };
-const fetchGenres = async (token: string | null) => {
-  var myHeaders = new Headers();
-
-  myHeaders.append("Accept", "application/json");
-  myHeaders.append("Authorization", `Bearer ${token}`);
-
-  var requestOptions = {
-    method: "GET",
-    headers: myHeaders,
-  };
-
-  return fetch(
-    `https://kata.conducerevel.com/films/genres`,
-    requestOptions
-  ).then((response) => response.json());
-};
+import MenuMovies from "./home/MenuMovies";
 
 export default function ListMovies() {
-  const [genres, setGenres] = useState([]);
-  const token: string | null = localStorage.getItem("token");
-  const handleSetGenres = (item: GenreType) => {
-    setGenres(item);
-  };
-  useEffect(() => {
-    fetchGenres(token).then((items) => {
-      setGenres(
-        items.sort((a: GenreType, b: GenreType) =>
-          a.name > b.name ? 1 : b.name > a.name ? -1 : 0
-        )
-      );
-    })
-  }, []);
   return (
     <>
-      {genres.map((genreItem: GenreType) => (
-        <ButtonFilter handleSetGenres={handleSetGenres} genre={genreItem} />
-      ))}
-      {genres.map((genreItem: GenreType) => (
-        <ListOfMovies genreObject={genreItem} />
-      ))}
+      <MenuMovies />
     </>
   );
 }
